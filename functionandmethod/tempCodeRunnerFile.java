a0721791763dc2b9bf6e190dee9806931290863c
goutamankit96-hub <goutamankit96@gmail.com>
2026-04-22 09:56:57 +0530

    final

--- functionandmethod/tempCodeRunnerFile.java
+++ functionandmethod/tempCodeRunnerFile.java
@@ -1,1 +1,11 @@
-public static void main(String[] args)
+public static boolean isPrime(int n){
+        for(int i=2; i<n-1;i++){   //completely dividing
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        System.out.println(isPrime(7));
+    }
