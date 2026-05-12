7f218817347f9bc82a263618bcb3c2fb396fae91
goutamankit96-hub <goutamankit96@gmail.com>
2026-05-12 13:31:31 +0530

    selection_sort

--- sorting/selection_sort.java
+++ sorting/selection_sort.java
@@ -5,7 +5,7 @@ public class selection_sort {
         for(int i=0;i<arr.length-1;i++){
             int minPos = i;
             for(int j=i+1;j<arr.length;j++){
-                if(arr[minPgit os]>arr[j]){
+                if(arr[minPos]>arr[j]){
                     minPos=j;
 
                 }
