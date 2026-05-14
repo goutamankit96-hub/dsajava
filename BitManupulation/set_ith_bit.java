67c274544cac914048286afbb7a5336668e53579
goutamankit96-hub <goutamankit96@gmail.com>
2026-05-14 13:14:40 +0530

    set_ith_bit

---
+++ BitManupulation/set_ith_bit.java
@@ -0,0 +1,12 @@
+public class set_ith_bit {
+    public static int SetIthBit(int n, int i){
+        int bitMask = 1<<i;
+        return n| bitMask;
+}
+    public static void main(String[] args) {
+        System.out.println(SetIthBit(10, 2));
+        
+    }
+    
+}
+    
