7428ca150dabb6e306364d6fdae2fbe2e27916c0
goutamankit96-hub <goutamankit96@gmail.com>
2026-05-15 17:16:55 +0530

    clear_last_ith_bit

---
+++ BitManupulation/clear_last_ith_bit.java
@@ -0,0 +1,10 @@
+public class clear_last_ith_bit {
+    public static int ClearLastIthBit(int n ,int i){
+        int bitMask=(~0)<<i;
+        return n & bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(ClearLastIthBit(15, 2));
+    }
+    
+}
