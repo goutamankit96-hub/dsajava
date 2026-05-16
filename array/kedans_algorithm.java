d668fde02b8659177e68fab68d7a71d3bee6ba8e
goutamankit96-hub <goutamankit96@gmail.com>
2026-05-16 16:23:18 +0530

    kedanes

---
+++ array/kedans_algorithm.java
@@ -0,0 +1,28 @@
+public class kedans_algorithm {
+   
+
+    public static void kedanes(int numbers[]) {
+        int cs = 0;
+
+        int ms = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            cs = cs + numbers[i];
+            if(cs < 0){
+                cs = 0;
+            }
+            ms = Math.max(cs,ms);
+    }
+    System.out.println("max sum = " + ms);
+}   
+
+
+    public static void main(String[] args) {
+
+        int numbers[] = {2, 4, 6};
+
+        kedanes(numbers);
+    }
+}
+    
+
