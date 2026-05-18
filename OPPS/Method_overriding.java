c506c9eed7d85913a725a559a4f5c1ed711f1534
goutamankit96-hub <goutamankit96@gmail.com>
2026-05-18 12:49:13 +0530

    Method_overriding

---
+++ OPPS/Method_overriding.java
@@ -0,0 +1,20 @@
+
+public class Method_overriding {
+    public static void main(String[] args) {
+        Deer d = new Deer();
+        d.eat();
+        
+    }
+    
+}
+class Animal {
+    void eat(){
+        System.out.println("eat anything");
+    }
+}
+class Deer extends Animal {
+    void eat(){
+         System.out.println("eat grass");
+        
+    }
+}
