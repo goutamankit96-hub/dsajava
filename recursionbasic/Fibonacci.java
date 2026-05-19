770b0b20c307063a748d0d8953178f5009cfc86f
goutamankit96-hub <goutamankit96@gmail.com>
2026-05-19 11:28:41 +0530

    fibonacci_number

---
+++ recursionbasic/Fibonacci.java
@@ -0,0 +1,21 @@
+public class Fibonacci {
+    public static int fib(int n){
+        //base case
+        if(n == 0 || n == 1){
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fib(n));
+        System.out.println(fib(22));
+        System.out.println(fib(23));
+        System.out.println(fib(24));
+        System.out.println(fib(25));
+    }
+    
+}
