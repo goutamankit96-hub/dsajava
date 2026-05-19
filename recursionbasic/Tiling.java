56eb05ecb4bbb09ce37f4d5ca845ac240688055a
goutamankit96-hub <goutamankit96@gmail.com>
2026-05-19 17:45:38 +0530

    Tiling_problem

---
+++ recursionbasic/Tiling.java
@@ -0,0 +1,23 @@
+public class Tiling {
+    public static int tilingProblem(int n){ //2 * n tiling
+        // Base case
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        //kaam
+        //verticle 
+        int fnm1 = tilingProblem(n-1);
+         
+        //horizontal
+        int fnm2 = tilingProblem(n-2);
+         
+        int totalWays = fnm1 + fnm2;
+        return totalWays;
+    }
+    public static void main(String[] args) {
+        System.out.println(tilingProblem(3));
+        System.out.println(tilingProblem(4));
+        
+    }
+    
+}
